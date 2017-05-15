41f498feb99ab36a3601bc639a58963f82a850ba
Viktoria1 <Viktoria.m.schreiner@gmail.com>
2017-05-15 23:16:59 +0200

    finished Exc 7; ToDo: change File-location to relative direction

---
+++ DynamischMitAjax/src/servlet/ServletListNames.java
@@ -0,0 +1,78 @@
+package servlet;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletListNames
+ */
+@WebServlet("/ServletListNames")
+public class ServletListNames extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletListNames() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String[] readLines(String filename) throws IOException {
+		Scanner sc = new Scanner(new File(filename));
+		List<String> lines = new ArrayList<String>();
+		while (sc.hasNextLine()) {
+			lines.add(sc.nextLine());
+		}
+
+		String[] arr = lines.toArray(new String[0]);
+		sc.close();
+		return arr;
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		String parameter = request.getParameter("wert");
+		System.out.println("Anfrage: " + parameter);
+		//System.out.println(System.getProperty("user.dir"));
+		String[] arr = readLines(
+				"C:\\Users\\Vk\\Studium\\4_Semester\\SoTe2\\Repository_sote2\\Softwaretechnik\\DynamischMitAjax\\WebContent\\Data\\vornamen.txt");
+
+		/*for (String name : arr) {
+			writer.write("<p> " + name + "</p>");
+		}
+		*/
+
+		for (String name : arr) {
+			boolean istrue = true;
+			if (parameter != "") {
+				int laenge = name.length() < parameter.length() ? name.length() : parameter.length();
+				for (int i = 0; i < laenge; i++) {
+					if (!(parameter.length() < i) && !(name.toLowerCase().charAt(i) == parameter.toLowerCase().charAt(i))){							
+						istrue = false;
+					}
+				}
+				if(istrue){
+				writer.write("<p> " + name + "</p>");
+				}
+			}
+		}
+
+		writer.flush();
+		writer.close();
+	}
+
+}
